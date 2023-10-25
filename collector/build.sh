53c223fe7e9c1e50e2c29bb7d67af52aa177749f
Mateusz Rumian <mrumian@sumologic.com>
2023-10-25 14:17:32 +0200

    chore(collector): fix build script

--- collector/build.sh
+++ collector/build.sh
@@ -8,8 +8,8 @@ popd || exit
 
 # Add config.yaml
 
-cp ../opentelemetry-lambda/collector/build/collector-extension-${GOARCH}.zip .
-unzip -qo collector-extension-${GOARCH}.zip
-rm collector-extension-${GOARCH}.zip
+cp ../opentelemetry-lambda/collector/build/opentelemetry-collector-layer-${GOARCH}.zip .
+unzip -qo opentelemetry-collector-layer-${GOARCH}.zip
+rm opentelemetry-collector-layer-${GOARCH}.zip
 cp ./config/config.yaml ./collector-config/config.yaml
 zip -r collector-layer.zip collector-config extensions
