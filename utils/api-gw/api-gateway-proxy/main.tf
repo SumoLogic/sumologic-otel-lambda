b2c3f7ef5183d3691f2838a559560605354af30e
Mateusz Rumian <mrumian@sumologic.com>
2023-10-27 15:34:35 +0200

    chore(tf): fix apigw lifecycle

--- utils/api-gw/api-gateway-proxy/main.tf
+++ utils/api-gw/api-gateway-proxy/main.tf
@@ -5,7 +5,10 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "api_resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "{proxy+}"
+  path_part   = var.name
+  lifecycle {
+    replace_triggered_by = [aws_api_gateway_rest_api.api.name]
+  }
 }
 
 resource "aws_api_gateway_method" "proxy_method" {
@@ -13,6 +16,9 @@ resource "aws_api_gateway_method" "proxy_method" {
   resource_id   = aws_api_gateway_resource.api_resource.id
   http_method   = "ANY"
   authorization = "NONE"
+  lifecycle {
+    replace_triggered_by = [aws_api_gateway_rest_api.api.name]
+  }
 }
 
 resource "aws_api_gateway_integration" "lambda" {
@@ -23,11 +29,21 @@ resource "aws_api_gateway_integration" "lambda" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = var.function_invoke_arn
+
+  lifecycle {
+    create_before_destroy = true
+    replace_triggered_by  = [aws_api_gateway_rest_api.api.name]
+  }
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on  = [var.lambda_function, aws_lambda_permission.lambda_api_allow_gateway, aws_api_gateway_integration.lambda]
   rest_api_id = aws_api_gateway_rest_api.api.id
+
+  lifecycle {
+    create_before_destroy = true
+    replace_triggered_by  = [aws_api_gateway_rest_api.api.name]
+  }
 }
 
 resource "aws_api_gateway_stage" "test" {
@@ -37,14 +53,13 @@ resource "aws_api_gateway_stage" "test" {
   xray_tracing_enabled = var.enable_xray_tracing
 
   lifecycle {
-    ignore_changes = [stage_name]
+    replace_triggered_by = [aws_api_gateway_rest_api.api.name]
   }
 }
 
 resource "aws_lambda_permission" "lambda_api_allow_gateway" {
   action        = "lambda:InvokeFunction"
   function_name = var.function_name
-  # qualifier = var.function_qualifier
-  principal  = "apigateway.amazonaws.com"
-  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
