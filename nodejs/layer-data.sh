3543a0bc1244e2ce887cea1a711d186d6dc03b7a
Mateusz Rumian <mrumian@sumologic.com>
2023-10-30 15:38:12 +0100

    chore(nodejs): update layer data

--- nodejs/layer-data.sh
+++ nodejs/layer-data.sh
@@ -3,7 +3,7 @@
 OFFICIAL_LAYER_NAME=sumologic-otel-lambda-nodejs
 ARCHITECTURE_AMD=x86_64
 ARCHITECTURE_ARM=arm64
-RUNTIMES='nodejs12.x nodejs14.x nodejs16.x nodejs18.x'
+RUNTIMES='nodejs14.x nodejs16.x nodejs18.x'
 DESCRIPTION='Sumo Logic OTel Collector and NodeJS Lambda Layer https://github.com/SumoLogic/sumologic-otel-lambda/tree/main/nodejs'
 LICENSE=Apache-2.0
-VERSION=v1-12-0
+VERSION=v1-17-1
