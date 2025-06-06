04d283d54d1176a08700a7f2ce3a509e157c3fa2
Cyril Cressent <ccressent@sumologic.com>
2025-06-06 12:39:50 -0700

    Update layer metadata

--- python/layer-data.sh
+++ python/layer-data.sh
@@ -3,7 +3,7 @@
 OFFICIAL_LAYER_NAME=sumologic-otel-lambda-python
 ARCHITECTURE_AMD=x86_64
 ARCHITECTURE_ARM=arm64
-RUNTIMES='python3.7 python3.8 python3.9 python3.10 python3.11'
-DESCRIPTION='Sumo Logic OTel Collector and Python Lambda Layer https://github.com/SumoLogic/sumologic-otel-lambda/tree/main/python'
+RUNTIMES='python3.9 python3.10 python3.11 python3.12 python3.13'
+DESCRIPTION='Sumo Logic OTEL Collector and Python Lambda Layer https://github.com/SumoLogic/sumologic-otel-lambda/tree/release-python-v1.32.0/python'
 LICENSE=Apache-2.0
-VERSION=v1-20-0
+VERSION=v1-32-0
